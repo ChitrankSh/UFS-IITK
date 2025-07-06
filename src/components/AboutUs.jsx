968d513c304cef67166183d46e7428d73aac9de2
Utkarsh-Aman <utkarsh4aman@gmail.com>
2025-07-06 16:47:17 +0530

    first commit

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./AboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <section id="about" className="about-section">
+      <h2>About Us</h2>
+      <p>
+        Welcome to the Ultimate Frisbee Society of IIT Kanpur! We are a vibrant
+        community of passionate players who enjoy playing, learning, and
+        competing in ultimate frisbee. Join us for thrilling matches, training
+        sessions, and national-level tournaments!
+      </p>
+    </section>
+  );
+};
+
+export default AboutUs;
